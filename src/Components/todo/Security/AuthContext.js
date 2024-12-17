8325f8a9a85b10f02c2298c34e8624a7ad842999
YashpalGawali <gawaliyashpal@gmail.com>
2024-12-17 17:43:12 +0530

    17 dec sec commit

--- src/Components/todo/Security/AuthContext.js
+++ src/Components/todo/Security/AuthContext.js
@@ -16,31 +16,38 @@ export default function AuthProvider( { children } ) {
 
     const [username , setUsername] = useState(null)
 
+    const [token , setToken] = useState(null)
    // const valueToBeShared = [number ,isAuthenticated ,setAuthenticated ]
 
     return(
         // <AuthContext.Provider value={{ valueToBeShared }}>
-        <AuthContext.Provider value={{ isAuthenticated ,setAuthenticated, login, logout, username }}>
+        <AuthContext.Provider value={{ isAuthenticated ,setAuthenticated, login, logout, username,token }}>
 
             {children}
         </AuthContext.Provider>
     )
 
 
-    function login(username , password) {
+    async function login(username , password) {
         const basicAuthToken = 'Basic '+ btoa(username +':'+password)
-        executeBasicAuthentication(basicAuthToken).then(response=> console.log(response)).catch(error=> console.log(error))
-        setAuthenticated(false)
-        // if(username==='admin' && password==='admin') {
-        //     setAuthenticated(true)
-        //     setUsername(username)
-        //     return true
-        // }
-        // else {
-        //     setAuthenticated(false)
-        //     setUsername(null)
-        //     return false
-        // }
+        try{
+            const response = await executeBasicAuthentication(basicAuthToken)
+                                        
+            if(response.status==200) {
+                setAuthenticated(true)
+                setUsername(username)
+                setToken(basicAuthToken)
+                return true
+            }
+            else {
+                logout()
+                return false
+            }
+        }
+        catch(error){
+           logout()
+            return false
+        }
     }
 
     // function login(username , password) {
@@ -58,5 +65,7 @@ export default function AuthProvider( { children } ) {
 
     function logout() {
         setAuthenticated(false) 
+        setToken(null)
+        setUsername(null)
     }
 }
